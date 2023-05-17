d7e278711cb8d21d27deb0f5605d5d53a4d24aa5
Sefa AŞIK <sefa.a1707@gmail.com>
2023-05-18 00:43:37 +0300

    Ders 5 sonu. AppUser ve AppRole sınıfları eklendi.

---
+++ EasyCashIdentityProjectDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EasyCashIdentityProjectEntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProjectDataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,Approle,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-I3NUK9S;initial catalog=EasyCashDb;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+
+
+
+    }
+}
